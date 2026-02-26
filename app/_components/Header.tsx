91cf6868d6f51a5f0e32ac83ff0a415aa0821339
lilKriT <lilKriTwebDev@gmail.com>
2026-02-26 21:52:55 +0100

    Mobile view

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className="z-10 fixed top-0 left-0 w-full flex justify-center items-center">
-      <div className="max-w-7xl w-full flex justify-between items-center mx-8 mt-16">
+      <div className="relative max-w-7xl w-full flex justify-between items-center mx-8 mt-16">
         {/* Logo */}
         <Image
           src={"/logo.svg"}
@@ -14,8 +19,8 @@ const Header = () => {
           className="h-10 w-auto"
         />
 
-        {/* Navigation */}
-        <nav>
+        {/* Navigation - Desktop */}
+        <nav className="hidden md:flex">
           <ul className="flex justify-center items-center gap-8">
             <li>
               <Link href={"/#"} className="navLink">
@@ -41,6 +46,67 @@ const Header = () => {
 
             <li>
               <Link href={"/#"} className="navLink navLink--faded">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Navigation - hamburger */}
+        <button
+          aria-label={open ? "Close menu" : "Open menu"}
+          aria-expanded={open}
+          aria-controls="mobile-menu"
+          className="md:hidden cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? (
+            <Image
+              src={"/icon-close.svg"}
+              alt="Menu hamburger"
+              width={24}
+              height={16}
+            />
+          ) : (
+            <Image
+              src={"/icon-hamburger.svg"}
+              alt="Menu hamburger"
+              width={24}
+              height={16}
+            />
+          )}
+        </button>
+
+        {/* Navigation - mobile */}
+        <nav
+          id="mobile-menu"
+          className={`${!open && "hidden"} md:hidden absolute left-0 right-0 top-full bg-white`}
+        >
+          <ul className="flex flex-col">
+            <li>
+              <Link href={"/#"} className="">
+                Product
+              </Link>
+            </li>
+
+            <li>
+              <Link href={"/#"} className="">
+                Features
+              </Link>
+            </li>
+
+            <li>
+              <Link href={"/#"} className="">
+                Pricing
+              </Link>
+            </li>
+
+            <li>
+              <div className="rounded-full bg-gray-300 w-2 aspect-square mt-1"></div>
+            </li>
+
+            <li>
+              <Link href={"/#"} className="">
                 Login
               </Link>
             </li>
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -3,13 +3,18 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <section className="min-h-dvh w-full flex justify-stretch items-stretch">
-      <div className="relative w-full grid grid-cols-[minmax(2rem,1fr)_minmax(20rem,40rem)_50vw]">
+      <div className="relative w-full grid grid-cols-1 lg:grid-cols-[minmax(2rem,1fr)_minmax(20rem,40rem)_50vw]">
         {/* Left margin, empty on purpose */}
 
-        <div></div>
+        <div className="hidden lg:block"></div>
+
+        {/* Right column */}
+        <div className="lg:order-2 relative min-h-140 bg-[url(/illustration-devices.svg)]  bg-no-repeat bg-size-[min(150%,100vh)] lg:bg-size-[130%] bg-position-[5vw_60%] lg:bg-position-[0_50%]">
+          <div className="absolute right-0 w-1/2 lg:w-full h-full lg:h-1/2 bg-blue-100 rounded-bl-[8vh] -z-10"></div>
+        </div>
 
         {/* Left column */}
-        <div className="flex flex-col justify-center pr-4 py-32">
+        <div className="lg:order-1 flex flex-col justify-center mx-8 lg:mx-0 lg:pr-4 pb-24 lg:py-32">
           <h2 className="uppercase mt-8 text-xl font-condensed text-gray-400 tracking-[.25rem]">
             <span className="text-white bg-blue-950 px-4 py-2 rounded-full mr-2 font-bold tracking-normal">
               New
@@ -17,7 +22,7 @@ const Hero = () => {
             Monograph Dashboard
           </h2>
 
-          <h1 className="text-7xl font-condensed font-bold uppercase mt-4">
+          <h1 className="text-[clamp(3.75rem,5vw,4.5rem)] font-condensed font-bold uppercase mt-4 leading-none text-balance">
             Powerful insights into your team
           </h1>
 
@@ -25,7 +30,7 @@ const Hero = () => {
             Project planning and time tracking for agile teams
           </p>
 
-          <div className="flex justify-start items-center gap-8 mt-16">
+          <div className="flex justify-start items-center gap-8 mt-8 lg:mt-16">
             <Link
               href={"/#"}
               className="bg-red-400 px-8 py-3 rounded-lg text-white uppercase font-condensed font-bold hover:bg-red-300 motion-safe:duration-150 ease-in-out"
@@ -36,11 +41,6 @@ const Hero = () => {
               to see a live preview
             </span>
           </div>
-        </div>
-
-        {/* Right column */}
-        <div className="relative bg-[url(/illustration-devices.svg)] bg-no-repeat bg-size-[130%] bg-position-[0_50%]">
-          <div className="absolute w-full h-1/2 bg-blue-100 rounded-bl-[4vw] -z-10"></div>
         </div>
       </div>
     </section>
